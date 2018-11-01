7e1bacaeaec3837452273a8f4e8399a853037fa3
kishan75 <kishan.chaurasiya.75@gmail.com>
2018-11-01 18:45:51 +0530

    modularization done

--- Taxcalculation.js
+++ Taxcalculation.js
@@ -6,12 +6,40 @@ var type = require('./Category.js');
 var Total = 0;
 var TotalSalesTax = 0;
 var Datawithstring = [];
+
+function preparing(data, price, tax) {
+    let prepare = data.join(' ');
+    prepare += ': ';
+    prepare += price + tax;
+    Datawithstring.push(prepare);
+}
+
+function Showdata() {
+    for (var j = 0; j < Datawithstring.length; j++)
+        console.log(Datawithstring[j]);
+    console.log("Sales Taxes: " + TotalSalesTax);
+    console.log("Total: " + Total);
+}
+
+function ImportCheack(data) {
+    if (data.indexOf("imported") == -1)
+        return false;
+    else
+        return true;
+
+}
+
+function ImportTax(price) {
+    return price * 0.05;
+}
+
+function SalesTax(price) {
+    return 0.1 * price;
+}
+
 rl.on('line', line => {
     if (line.length == 0) {
-        for (var j = 0; j < Datawithstring.length; j++)
-            console.log(Datawithstring[j]);
-        console.log("Sales Taxes: " + TotalSalesTax);
-        console.log("Total: " + Total);
+        Showdata();
         rl.close();
     } else {
         var Data = line;
@@ -21,17 +49,12 @@ rl.on('line', line => {
         Data.pop();
         Data.pop();
         var Tax = 0;
-        if (Data.indexOf("imported") != -1)
-            Tax += (0.05 * Price);
-        if (type(Data) == undefined) {
-            Tax += (0.1 * Price);
-            TotalSalesTax += (0.1 * Price);
-        }
-        Total += (Price + Tax);
-        var prepare = Data.join(' ');
-        prepare += ': ';
-        prepare += Price + Tax;
-        Datawithstring.push(prepare);
-
+        if (ImportCheack(Data) == true)
+            Tax += ImportTax(Price);
+        if (type(Data) == undefined) 
+            Tax += SalesTax(Price),TotalSalesTax += SalesTax(Price);
+        
+       Total += (Price + Tax);
+        preparing(Data, Price, Tax);
     }
 });
